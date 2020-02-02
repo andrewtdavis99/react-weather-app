d4f720079b610c21d293820848113b3ab367f227
Andrew Davis <andrewtdavis@hotmail.co.uk>
2020-02-02 15:43:17 +0000

    Updates

---
+++ precache-manifest.c03e08f7d1011b365a8593eedb0f023f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91bebd3dc622ede1e12c037cc072cabe",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "406e023bad0603a70ee9",
+    "url": "/react-weather-app/static/css/2.acf6554b.chunk.css"
+  },
+  {
+    "revision": "69077286e7a3be3d7af7",
+    "url": "/react-weather-app/static/css/main.b12781c5.chunk.css"
+  },
+  {
+    "revision": "406e023bad0603a70ee9",
+    "url": "/react-weather-app/static/js/2.5c213e1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-weather-app/static/js/2.5c213e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69077286e7a3be3d7af7",
+    "url": "/react-weather-app/static/js/main.73f3a068.chunk.js"
+  },
+  {
+    "revision": "d2857d5f4a409e252419",
+    "url": "/react-weather-app/static/js/runtime-main.a75341be.js"
+  },
+  {
+    "revision": "2eeb1fe6d168de456985db312e5a7f3a",
+    "url": "/react-weather-app/static/media/Weather-data-background.2eeb1fe6.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
